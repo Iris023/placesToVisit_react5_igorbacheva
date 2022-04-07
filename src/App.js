2e1e3a31e81da345418b7bddd5d02913669aa7a3
Iris🌸 <Iriska@IrisMacBook.local>
2022-04-07 14:18:28 +0300

    4alla

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
   const [additionalPic, setAdditionalPic] = useState(0);
   const {src, name} = pictures[additionalPic];
 
+  if(placesList.length === 0) {
+    document.querySelector('h1').textContent = 'Oops.. There are no places for you to Visit';
+    document.querySelector('.DeleteAll').classList.add('hidden');
+    document.querySelector('.restartBtn').classList.remove('hidden');
+  }
+
   const showTextClick = (item) => {
     item.showMore = !item.showMore;
     setTextLength(!textLength);
@@ -21,6 +27,10 @@ function App() {
 
   const deleteAllPlaces = () => {
     setPlacesList([]);
+  }
+
+  const restart = () => {
+    document.location.assign("https://react5-top10places-igorbacheva.netlify.app");
   }
 
   const nextPic = () => {
@@ -48,27 +58,28 @@ function App() {
     <div className="container">
       <h1>Top {placesList.length} Places You Should Visit in Europe</h1>
 
-    {placesList.map((i => {
-      const {id, city, country, img, info, showMore} = i;
+      {placesList.map((i => {
+        const {id, city, country, img, info, showMore} = i;
 
-      return (
-        <div>
-            <div key={id} className="container">
-              <h2>{city}, {country}</h2>
-              <img src={img} alt='city' className='city_pic'/>
-              <p>{showMore ? info : info.substring(0,200) + '...'}
-              <button className='showMoreBtn' onClick={() =>showTextClick(i)}>{showMore ? "Show less" : "Show more"}</button>
-              </p>
-              <button className='deleteEach' onClick={() => removePlace(id)}>I've already been there</button>
-            </div>
-        </div>
-      )
-    }))}
+        return (
+          <div>
+              <div key={id} className="container">
+                <h2>{city}, {country}</h2>
+                <img src={img} alt='city' className='city_pic'/>
+                <p>{showMore ? info : info.substring(0,200) + '...'}
+                <button className='showMoreBtn' onClick={() =>showTextClick(i)}>{showMore ? "Show less" : "Show more"}</button>
+                </p>
+                <button className='deleteEach' onClick={() => removePlace(id)}>I've already been there</button>
+              </div>
+          </div>
+        )
+      }))}
 
     <button className='DeleteAll' onClick={deleteAllPlaces}>I don't want to go anywhere</button>
+    <button className='restartBtn hidden' onClick={restart}>Try again</button>
 
 
-      <div className='container howToDeletr'>
+      <div className='container'>
         <h3>To see more pictures click the buttons below</h3>
         <img className='city_pic' alt="city" src={src}/>
         <p>{name}</p>
