78295a624a79fe65b9bac8f6dcd27df19639ab5f
Iris🌸 <Iriska@IrisMacBook.local>
2022-11-08 00:06:33 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ function App() {
   const [additionalPic, setAdditionalPic] = useState(0);
   const {src, name} = pictures[additionalPic];
 
-  if(placesList.length === 0) {
-    document.querySelector('h1').textContent = 'Oops.. There are no places for you to Visit';
-    document.querySelector('.DeleteAll').classList.add('hidden');
-    document.querySelector('.restartBtn').classList.remove('hidden');
-  }
-
   const showTextClick = (item) => {
     item.showMore = !item.showMore;
     setTextLength(!textLength);
@@ -25,13 +19,6 @@ function App() {
     setPlacesList(newList);
   }
 
-  const deleteAllPlaces = () => {
-    setPlacesList([]);
-  }
-
-  const restart = () => {
-    document.location.assign("https://react5-top10places-igorbacheva.netlify.app");
-  }
 
   const nextPic = () => {
     setAdditionalPic(( additionalPic => {
@@ -54,16 +41,20 @@ function App() {
   }
 
 
+
   return (
     <div className="container">
-      <h1>Top {placesList.length} Places You Should Visit in Europe</h1>
+  
+  {
+  placesList.length === 0 ? <h1>Oops.. There are no places for you to Visit</h1>  : <h1>Top {placesList.length} Places You Should Visit in Europe</h1>
+  }
 
       {placesList.map((i => {
         const {id, city, country, img, info, showMore} = i;
 
         return (
-          <div>
-              <div key={id} className="container">
+          <div key={id}>
+              <div className="container">
                 <h2>{city}, {country}</h2>
                 <img src={img} alt='city' className='city_pic'/>
                 <p>{showMore ? info : info.substring(0,200) + '...'}
@@ -75,17 +66,16 @@ function App() {
         )
       }))}
 
-    <button className='DeleteAll' onClick={deleteAllPlaces}>I don't want to go anywhere</button>
-    <button className='restartBtn hidden' onClick={restart}>Try again</button>
-
+    <button className={placesList.length === 0 ? 'restartBtn' : 'DeleteAll'} onClick={placesList.length === 0 ? ()=> setPlacesList(data) : ()=> setPlacesList([])}>{placesList.length === 0 ? 'Show again' : `I don't want to go anywhere`}</button>
+  
 
       <div className='container'>
         <h3>To see more pictures click the buttons below</h3>
         <img className='city_pic' alt="city" src={src}/>
         <p>{name}</p>
         <div>
-          <button className='slider' onClick={prevPic}>Prev</button>
-          <button className='slider' onClick={nextPic}>Next</button>
+          <button className='slider' onClick={prevPic}>← Prev</button>
+          <button className='slider' onClick={nextPic}>Next →</button>
         </div>
       </div>
 
